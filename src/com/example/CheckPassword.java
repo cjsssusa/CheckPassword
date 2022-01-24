67fd9da6e746311bf45050e37b67b33462e3e300
cjsssusa <cjsssusa@gmail.com>
2022-01-24 14:38:19 -0500

    with regex

--- src/com/example/CheckPassword.java
+++ src/com/example/CheckPassword.java
@@ -1,28 +1,27 @@
 package com.example;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CheckPassword {
 
     public static boolean checkPassword(String pwd) {
 
         int sl = pwd.length();
         if (sl < 4 || sl > 6) return false;
-        for (int i = 0; i < sl; i++) {
-            if (Character.isDigit(pwd.charAt(i)) ) {
-                ;
-            } else if(Character.isSpaceChar(pwd.charAt(i))) {
-                return false;
-            }
-            else{
-                return false;
-            }
-        }
-        return true;
+
+        Pattern r = Pattern.compile("^\\d{4,6}$");
+        Matcher m = r.matcher(pwd);
+        return m.find();
     }
 
     public static void main(String[] args) {
 
-//        System.out.println(checkPassword("121317"));
-//        System.out.println(checkPassword(" 1234"));
-        System.out.println(checkPassword("89abc1"));
+        System.out.println(checkPassword("121317"));
+        System.out.println(checkPassword(" 1234"));
+        System.out.println(checkPassword(""));
+        System.out.println(checkPassword("41234321"));
+        System.out.println(checkPassword("41234"));
+        System.out.println(checkPassword("4wq34"));
     }
 }
